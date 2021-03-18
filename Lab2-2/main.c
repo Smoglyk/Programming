5edc4ec1e02cc0fc16dedcf8bb4915bf82034ee0
Smoglyk <76232699+Smoglyk@users.noreply.github.com>
2021-03-18 03:23:30 +0300

    Add files via upload

---
+++ Lab2-2/main.c
@@ -0,0 +1,33 @@
+
+#include <math.h>
+#include "main.h"
+
+int fact(int i)
+{
+  if(i==1) return(i);
+  return i * fact(i - 1);
+}
+
+
+
+int main(){
+
+float e, x;
+int n;
+double comparition, talor;
+
+scanf("%f", &x);
+scanf("%f", &e);
+
+n=1;
+
+do 
+{
+    talor+=pow((-1),n-1)*(pow(x,2*n-1)/fact(2*n-1));
+    comparition=fabs(sin(x)-talor);
+    n++;
+}  while(comparition>e);
+n -= 1;
+printf("%d",n);
+
+}
---
+++ Lab2-2/main_test.c
@@ -0,0 +1,17 @@
+
+#include "main.h"
+
+int test_fact() {
+    assert(fact(5) == 120);
+    assert(fact(6) == 720);
+}
+
+#undef main
+
+int main() {
+
+   test_fact();
+    printf("Test done true");
+    return 0;
+
+}
