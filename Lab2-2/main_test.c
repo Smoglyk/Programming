64b2b931202ae70cc194686eeb4445c37600368f
Smoglyk <76232699+Smoglyk@users.noreply.github.com>
2021-03-18 03:30:29 +0300

    Add files via upload

---
+++ Lab2-2/main_test.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+
+void test_fact() {
+    assert(fact(5) == 120);
+    assert(fact(6) == 720);
+    
+}
+
+#undef main
+
+int main() {
+
+   test_fact();
+    printf("Test done true");
+    return 0;
+
+}
