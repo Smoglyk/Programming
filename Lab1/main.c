a3656769eea148dab5ebdc5823c62e58352eb296
Smoglyk <76232699+Smoglyk@users.noreply.github.com>
2021-03-23 02:16:37 +0300

    Update main.c

--- Lab1/main.c
+++ Lab1/main.c
@@ -1,12 +1,24 @@
-#include <stdio.h>
+
 #include "main.h"
+
+int Correctinput()
+{
+    int temp;
+    char goodCheck;
+    while(!scanf("%d", &temp) || !scanf("%c", &goodCheck) || goodCheck != '\n')
+    {
+       printf("Wrong input! Please, try again: ");
+       fflush(stdin);
+    }
+    return temp;
+}
 int checkDigital(double xt)
 {
     if(xt < 0)
     {
         return 0;
     }
-   return 1;
+ return 1;
 }
 
 int fact(int a)
@@ -23,22 +35,23 @@ int fact(int a)
 int main()
 {
 int i = 1, j = 1, n, resultSum = 0, resultMulti = 1;
-scanf("%d", &n);
+n = Correctinput();
 checkDigital(n);
-while(i <= n)
-{
-    while(j <= i)
-    {
-        int copy_p = resultMulti;
-        resultMulti = fact(j+i)/ fact(i);
-        resultMulti *= copy_p;
-        j++;
-    }
+    while(i <= n)
+   {
+       while(j <= i)
+      {
+          int copy_p = resultMulti;
+          resultMulti = fact(j+i)/ fact(i);
+          resultMulti *= copy_p;
+          j++;
+      }
     resultSum += resultMulti;
     resultMulti = 1;
     i++;
     j = 1; 
-}
+   }  
 
-printf("Answer : %d", resultSum);
+   printf("Answer : %d", resultSum);
+
 }
