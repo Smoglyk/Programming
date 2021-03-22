3fac884ab099f5800dd50ead4b12aeda629074ac
Smoglyk <76232699+Smoglyk@users.noreply.github.com>
2021-03-23 02:21:05 +0300

    Update main_test.c

--- Lab2-2/main_test.c
+++ Lab2-2/main_test.c
@@ -1,17 +1,19 @@
 
 #include "main.h"
 
-void test_fact() {
+void test_fact() 
+{
+    int check = Correctinput();
     assert(fact(5) == 120);
     assert(fact(6) == 720);
-    
+    assert(check || check == 0);
 }
 
 #undef main
 
 int main() {
 
-   test_fact();
+    test_fact();
     printf("Test done true");
     return 0;
 
