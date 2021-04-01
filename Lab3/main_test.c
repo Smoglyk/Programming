95ff488c16fd4b074084fc238d2a78639ce95ccd
Smoglyk <76232699+Smoglyk@users.noreply.github.com>
2021-04-01 10:19:39 +0300

    Update main_test.c

--- Lab3/main_test.c
+++ Lab3/main_test.c
@@ -2,8 +2,28 @@
 
 void test_cheakInput()
 {
+    int sizeX = 8, sizeY = 8, sizeZ = 8, i, j;
+    int ***Cube = (int***)malloc(sizeX * sizeof(int**));
+    for (i = 0; i < sizeX; i++)
+	{
+        Cube[i] = (int**)malloc(sizeY * sizeof(int*));
+        for (j = 0; j < sizeY; i++)
+	        Cube[i][j] = (int*)malloc(sizeZ * sizeof(int));
+    }
+
     assert(Input01(1) == 1);
     assert(Input01(5) == 0);
+    assert(Check_Empty(Cube, sizeX, sizeY, sizeZ) == 1);
+
+    for (i = 0; i < sizeX; i++)
+	{
+        for (j = 0; j < sizeY; i++)
+	         free(Cube[i][j]);
+    }
+    for (i = 0; i < sizeY; i++)
+	         free(Cube[i]);
+    
+    free(Cube);
     
 }
 
