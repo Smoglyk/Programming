722ee2d3782c8658be0b9e724e1a3d4d84b3df94
Smoglyk <76232699+Smoglyk@users.noreply.github.com>
2021-04-13 15:10:18 +0300

    Update main_test.c

--- Lab4_1/main_test.c
+++ Lab4_1/main_test.c
@@ -4,9 +4,13 @@ void Check_Test()
 {
     int i, j, sizeX = 2, sizeY = 8;
     char**Arr = (char**)malloc(sizeX * sizeof(char**));
+	if(Arr == 0)
+		exit(1);
    for(i = 0; i < sizeX; i++)
    {
        Arr[i] = (char*)malloc(sizeY * sizeof(char));
+	   if(Arr[i] == 0)
+		exit(1);
    }
 
    assert(Check_Empty(Arr, sizeX) == 1);
